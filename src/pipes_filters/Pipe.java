883c545bc41a67cb71c29538f3c97224cf9bf02c
Fidac <ernestquevcab@gmail.com>
2021-01-30 15:31:48 -0600

    Implemented read and write functions of the Pipe class.

--- src/pipes_filters/Pipe.java
+++ src/pipes_filters/Pipe.java
@@ -1,7 +1,30 @@
 package pipes_filters;
+import java.io.IOException;
 import java.io.PipedReader;
+import java.io.PipedWriter;
 
 public class Pipe {
 	private PipedReader reader;
-	private PipedReader writer;
+	private PipedWriter writer;
+	
+	Pipe() throws IOException{
+		writer = new PipedWriter();
+		reader = new PipedReader(writer);
+	}
+	
+	public void write(String file) throws IOException {
+		writer.write(file);
+	}
+	
+	public String read() throws IOException {
+		int data = reader.read();
+		StringBuilder sb = new StringBuilder();
+		while(data != -1) {
+			sb.append((char) data);
+			data = reader.read();
+		}
+		
+		return sb.toString();
+	}
+	
 }
