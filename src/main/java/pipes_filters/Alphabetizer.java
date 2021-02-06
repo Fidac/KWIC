0606c7cbbc42af30819b42f37cc0dd8aad35b6a4
Jingya_Wang1 <jingya_wang1@baylor.edu>
2021-02-06 14:03:59 -0600

    Implement the alphabetizer and output; Write a simple test case.

--- src/main/java/pipes_filters/Alphabetizer.java
+++ src/main/java/pipes_filters/Alphabetizer.java
@@ -13,16 +13,17 @@ public class Alphabetizer extends Filter {
 	@Override
 	public void run() {
 		try {
-			System.out.println("Alphabetizer: ");
+			System.out.println("Alphabetizer: sort the sentences");
 			String[] shiftedLines = input.read().trim().split("\\n");
 			Utils.RemoveChangeOfLineChars(shiftedLines);
 
 			Arrays.sort(shiftedLines);
-			System.out.println("Lines...");
-			for(int i = 0; i < shiftedLines.length; ++i) {
-				System.out.println("Line: " + shiftedLines[i]);
+			StringBuilder sb = new StringBuilder();
+			for(String str : shiftedLines) {
+				sb.append(str).append("\n");
 			}
-			
+			output.write(sb.toString());
+
 			// call methods and execute logic that sort the shiftedLines alphabetically
 			// write to the output pipe the orderedLines
 			// output.write(orderedLines)
--- src/test/java/pipes_filters/ExampleTest.java
+++ src/test/java/pipes_filters/ExampleTest.java
@@ -1,4 +1,13 @@
 package pipes_filters;
 
+import org.junit.jupiter.api.Test;
+
 public class ExampleTest {
+    @Test
+    public void testAlphabetizer() throws Exception{
+        String file = "D:\\JavaWork\\KWIC-main\\KWIC\\files\\file.txt";
+        KWIC kwic = new KWIC();
+        kwic.execute(file);
+    }
+
 }
